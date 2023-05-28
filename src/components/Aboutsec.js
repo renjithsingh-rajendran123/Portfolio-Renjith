da23e3a218505b3933246d8539c1b35352ccc903
renjithsingh-rajendran123 <40191389+renjithsingh-rajendran123@users.noreply.github.com>
2023-05-28 19:34:45 +0530

    Update Aboutsec.js

--- src/components/Aboutsec.js
+++ src/components/Aboutsec.js
@@ -5,14 +5,14 @@ import { useEffect, useState } from "react";
 const Aboutsec= () => {
     const [experience, setExp] = useState(0); 
     useEffect(()=>{
-        setExp(new Date().getFullYear() - 2016);
+        setExp(new Date().getFullYear() - 2017);
     },[]);
     return(<>
             <div className='title' id="about">About Renjith,</div>
             <div className='about-sec'>
                 <div className='about-sec-sub-one'>
                     <p>I have completed my Bachelors degree in Information Technology from Anna University, Tirunelveli.</p>
-                    <p>I'm a front-end web developer with a work experience of {experience} years. Currently, working with Standard Chartered Bank.</p>
+                    <p>I'm a front-end web developer with a work experience of {experience}+ years. Currently, working with Standard Chartered Bank.</p>
                 </div>
                 <div className='about-sec-sub-two'>
                     <p>Hands on experience in below technologies,<br></br></p>
