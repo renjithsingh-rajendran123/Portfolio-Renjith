6ad73ddaabcab9c111d18acaeb11cd7f83896c2c
renjithsingh-rajendran123 <singhrenji@gmail.com>
2023-06-21 18:16:28 +0530

    analytics

--- src/App.js
+++ src/App.js
@@ -9,8 +9,14 @@ import Welcomesec from './components/Welcomesec';
 import Aboutsec from './components/Aboutsec';
 import Projectssec from './components/Projectssec'; 
 import Contactsec from './components/Contactsec';
+import ReactGA from 'react-ga';
+import {useEffect} from 'react';
 
 function App() { 
+    useEffect(() => {
+        ReactGA.initialize('G-RFP78SD795');
+        ReactGA.pageview(window.location.pathname + window.location.search)
+    },[]);
     return (
         <div className="App">
             <Welcomesec></Welcomesec> 
